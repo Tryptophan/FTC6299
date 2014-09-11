839dc2143b78d66be90545954d86569083e842b2
tryptophan <jagreenway12@gmail.com>
2014-09-11 16:36:57 -0500

    fixed gyro methods

--- libs.h
+++ libs.h
@@ -32,15 +32,16 @@ void stopMotors() {
 	motor[motorBR] = 0;
 }
 
-void moveTo(int power, int deg, float threshold = 2.0, long time = 30000) {
+void moveTo(int power, int deg, float threshold = 2.0, long time = 5000) {
 	heading = 0;
-	nMotorEncoder[motorBL] = 0;
-	nMotorEncoder[motorBR] = 0;
-	clearTimer(T1);
+	nMotorEncoder[motorFL] = 0;
+	nMotorEncoder[motorFR] = 0;
 
 	wait1Msec(500);
 	HTGYROstartCal(SENSOR_GYRO);
 	wait1Msec(500);
+
+	clearTimer(T1);
 
 	if (power > 0) {
 		while (time1[T1] < time && getEncoderAverage(nMotorEncoder[motorFL], nMotorEncoder[motorBL]) < deg) {
@@ -55,10 +56,10 @@ void moveTo(int power, int deg, float threshold = 2.0, long time = 30000) {
 			// If not, lower the speed of the required side of the robot to adjust back to 0
 			else {
 				if (heading > 0) {
-					setMotors(30, 50);
+					setMotors((power / 4) * getDriveDir(power), power);
 				}
 				if (heading < 0) {
-					setMotors(50, 30);
+					setMotors(power, (power / 4) * getDriveDir(power));
 				}
 			}
 
@@ -66,10 +67,10 @@ void moveTo(int power, int deg, float threshold = 2.0, long time = 30000) {
 		}
 	}
 
-	/*else {
+	else {
 		while (time1[T1] < time && getEncoderAverage(nMotorEncoder[motorFL], nMotorEncoder[motorFR]) > deg) {
 			// Reads gyros rate of turn, mulitplies it by the time passed (20ms), and adds it to the current heading
-			heading += valInRange(HTGYROreadRot(SENSOR_GYRO), threshold) * (float)(20 / 1000.0);
+				heading += valInRange(HTGYROreadRot(SENSOR_GYRO), threshold) * (float)(20 / 1000.0);
 
 			// Checks if the gyro is outside of the specified threshold (1.0)
 			if (isInRange(heading, 0, threshold)) {
@@ -78,27 +79,33 @@ void moveTo(int power, int deg, float threshold = 2.0, long time = 30000) {
 
 			// If not, lower the speed of the required side of the robot to adjust back to 0
 			else {
-				if (heading > 0)
+				if (heading > 0) {
 					setMotors(power, (power / 4) * getDriveDir(power));
-				else
+				}
+				if (heading < 0) {
 					setMotors((power / 4) * getDriveDir(power), power);
+				}
 			}
 
 			wait1Msec(20);
 		}
-	}*/
+	}
 
 	stopMotors();
 }
 
-void turn(int power, int deg, int time = 2000) {
+void turn(int power, int deg, int time = 5000) {
 	// 90 Degree Modifier
-	int modifier = deg * 7/9;
+	int modifier = deg * 8/9;
 	deg = modifier;
 
 	heading = 0;
-	clearTimer(T1);
+
+	wait1Msec(500);
 	HTGYROstartCal(SENSOR_GYRO);
+	wait1Msec(500);
+
+	clearTimer(T1);
 
 	if (deg > 0) {
 		while (time1[T1] < time && heading < deg) {
