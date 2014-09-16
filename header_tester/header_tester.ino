8c16135b7c48feb41ff13364aee8ebae8c580467
linnealovespie <linnealovespie@gmail.com>
2014-09-16 08:36:54 -0500

    getHeading()

--- header_tester/header_tester.ino
+++ header_tester/header_tester.ino
@@ -1,10 +1,9 @@
 #include"Wire.h"
-#include"Timer.h"
 
-const int MPU=0x68;  // I2C address of the MPU-6050
+const int MPU=0x68;  // I2C address of the MPU-6050 
 
-int heading, GyX; 
-Timer t; 
+int heading = 0; 
+int GyX; 
 
 void setup()
 {
@@ -13,26 +12,22 @@ void setup()
   Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
-  Serial.begin(9600);
-
+  Serial.begin(38400);
 }
 
 //default at power up: 250 degress/second
 //MPU6050_write_reg (MPU6050_PWR_MGMT_1, 0);
 void loop()
 {
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  Serial.print(getHeading);
+  //GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  Serial.println(getHeading());
 }
 
 int getHeading()
 {
-  heading = 0;
-  while(true)
-  {
-    heading += GyX * (float) (20/1000);
-    delay(20);
-  }
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  heading += GyX * (float) (20/1000);
+  delay(20);
   return heading;
 }
 
