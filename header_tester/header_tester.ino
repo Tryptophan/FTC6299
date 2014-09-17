dd63f8e6a5869c714a36f16905a47b5336cbdabc
linnealovespie <linnealovespie@gmail.com>
2014-09-16 19:01:30 -0500

    *minion voice* whaaaaaa?
    this might take some research. either that or i'm dumb

--- header_tester/header_tester.ino
+++ header_tester/header_tester.ino
@@ -1,33 +1,35 @@
 #include"Wire.h"
 
 const int MPU=0x68;  // I2C address of the MPU-6050 
-
+#define MPU6050_GYRO_ZOUT_H        0x47   // R
 int heading = 0; 
-int GyX; 
+int GyZ; 
 
-void setup()
-{
+void setup(){
   Wire.begin();
   Wire.beginTransmission(MPU);
   Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
-  Serial.begin(38400);
+  Serial.begin(9600);
 }
 
-//default at power up: 250 degress/second
-//MPU6050_write_reg (MPU6050_PWR_MGMT_1, 0);
 void loop()
 {
-  //GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  Wire.beginTransmission(MPU);
+  Wire.write(0x47);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,2,true);  // request a total of 14 registers
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Serial.println(" | GyZ = "); Serial.println(GyZ);
   Serial.println(getHeading());
+  delay(300);
 }
 
 int getHeading()
 {
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  heading += GyX * (float) (20/1000);
-  delay(20);
+  heading += GyZ * (float) (20/1000);
   return heading;
+  delay(20);
 }
 
