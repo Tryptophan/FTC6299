9f7bd2c2643bc2a925a92103a2531f2ba3e42140
linnealovespie <linnealovespie@gmail.com>
2014-09-24 16:57:05 -0500

    interrupts

--- header_tester/arduinoSideofComm/arduinoSideofComm.ino
+++ header_tester/arduinoSideofComm/arduinoSideofComm.ino
@@ -1,4 +1,9 @@
-int command, data, request;
+#define DATA_PIN_0 8 //Ports 8 - 13 as well as A0/1 are data pins
+#define DATA_WIDTH 8
+#define CMD_PIN_0 4 //Ports 4 - 7 are command pins
+#define WR_INT 3 //The "WR" port on SP for sending pulse is port 3 on Arduino
+
+int command, data, request, heading, zRaw;
 
 void setup()
 {
@@ -8,24 +13,49 @@ void hiSP()
 {
   command = 0;
   data = 0;
-  if(command <= 2)
+  
+  //If the command is perform something from SP
+  if (command <= 2)
   {
      for (int i = 0; i < 4; i++)
     {
-      command += digitalRead(4+i) << i;
+      command += digitalRead(CMD_PIN_0 + i) << i;
     }
-    for (int i - 0; i < 8; i++)
-    {
-      data += digitalRead(DATA_PIN_0 + i) << i;
-    }
-    request = 1;
   }
   
-  if (command >
+  //If the commands are to send data back to the SP
+  if (command > 2)
+  {
+    switch (command)
+    {
+      case 3:
+        data = heading;
+        break;
+      case 4:
+        data = zRaw; 
+        break;
+    }
+    
+    for (int i = 0; i < DATA_WIDTH; i++)
+    {
+      pinMode(DATA_PIN_0 + i, OUTPUT);
+      if (data & (1 << i))
+      {
+        digitalWrite(DATA_PIN_0 + i, HIGH);
+      }
+      else
+        digitalWrite(DATA_PIN_0 + i, LOW);
+    }
+    
+    for (int i = 0; i < DATA_WIDTH; i++)
+    {
+      pinMode(DATA_PIN_0 + i, INPUT);
+    }
+  }
 }
+
 void loop()
 {
-  if(attachInterrupt(3, hiSP, RISING))
-  {}
+  attachInterrupt(WR_INT, hiSP, RISING);
 }
 
