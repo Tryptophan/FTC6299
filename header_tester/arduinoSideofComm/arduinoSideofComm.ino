91ec07809f67ca35163a4419b53fa4efe3e1bb0a
linnealovespie <linnealovespie@gmail.com>
2014-09-30 15:58:20 -0500

    silly me... gotta read command first

--- header_tester/arduinoSideofComm/arduinoSideofComm.ino
+++ header_tester/arduinoSideofComm/arduinoSideofComm.ino
@@ -3,6 +3,7 @@
 #define DATA_PIN_0 8 //Ports 8 - 13 as well as A0/1 are data pins
 #define DATA_WIDTH 8
 #define CMD_PIN_0 4 //Ports 4 - 7 are command pins
+#define CMD_WIDTH 4 //Width of command pins
 #define WR_INT 3 //The "WR" port on SP for sending pulse is port 3 on Arduino
 
 int command, data, request, heading, GyZ;
@@ -24,42 +25,26 @@ void hiSP()
   data = 0;
   
   //If the command is perform something from SP
-  if (command <= 2)
+  for (int i = 0; i < CMD_WIDTH; i++)
   {
-     for (int i = 0; i < 4; i++)
+    command += digitalRead(CMD_PIN_0 + i) << i;
+  }
+
+  for (int i = 0; i < DATA_WIDTH; i++)
+  {
+    pinMode(DATA_PIN_0 + i, OUTPUT);
+    if (data & (1 << i))
     {
-      command += digitalRead(CMD_PIN_0 + i) << i;
+      digitalWrite(DATA_PIN_0 + i, HIGH);
+    }
+    else
+    {
+      digitalWrite(DATA_PIN_0 + i, LOW);
     }
   }
-  
-  //If the commands are to send data back to the SP
-  if (command > 2)
+  for (int i = 0; i < DATA_WIDTH; i++)
   {
-    switch (command)
-    {
-      case 3:
-        data = heading;
-        break;
-      case 4:
-        data = GyZ; 
-        break;
-    }
-    
-    for (int i = 0; i < DATA_WIDTH; i++)
-    {
-      pinMode(DATA_PIN_0 + i, OUTPUT);
-      if (data & (1 << i))
-      {
-        digitalWrite(DATA_PIN_0 + i, HIGH);
-      }
-      else
-        digitalWrite(DATA_PIN_0 + i, LOW);
-    }
-    
-    for (int i = 0; i < DATA_WIDTH; i++)
-    {
-      pinMode(DATA_PIN_0 + i, INPUT);
-    }
+    pinMode(DATA_PIN_0 + i, INPUT);
   }
 }
 
