5077b51d8215816d5d192e5d25db2c9a85ed9151
linnealovespie <linnealovespie@gmail.com>
2014-10-15 14:14:23 -0500

    noo autonymouse

---
+++ autonomous_chooser.h
@@ -0,0 +1,126 @@
+#include "drivers\JoystickDriver.c";
+#include "autonomous_methods.h";
+#include "libs.h"
+
+//autonomous_chooser.h
+
+int i, j, begin, suspend = 0;
+string sides[2] = {"Ramp", "Zone"};
+string side = sides[0];
+
+string programs[3] = {"Autonomous", "autoBDump", "test"};
+string file = programs[0];
+
+int toggle = 0;
+
+void startupCheck(){
+	if (HTGYROreadRot(SENSOR_GYRO) == 0){
+		playSound(soundException);
+		displayTextLine(6, "Check Gyro");
+	}
+}
+
+void toggleMenu() {
+	toggle++;
+	if (toggle > 2) {
+		toggle = 0;
+		wait1Msec(250);
+		return;
+	}
+	wait1Msec(250);
+}
+
+void toggleSubMenu(int dir) {
+	switch(toggle) {
+		case 0 :
+			// Select Right
+			if (dir == 1 && i < 2) {
+				i++;
+				file = programs[i];
+				wait1Msec(250);
+			}
+
+			// Select Left
+			else if (dir == 2 && i > 0) {
+				i--;
+				file = programs[i];
+				wait1Msec(250);
+			}
+			break;
+
+		case 1 :
+			// Select Right
+			if (dir == 1) {
+				suspend++;
+				wait1Msec(250);
+			}
+
+			// Select Left
+			else if (dir == 2 && suspend > 0) {
+				suspend--;
+				wait1Msec(250);
+			}
+			break;
+
+		case 2 :
+			// Select Right
+			if (dir == 1 && j < 2) {
+				j++;
+				side = sides[j];
+				begin++;
+				wait1Msec(250);
+			}
+
+			// Select Left
+			else if (dir == 2 && j > 0) {
+				j--;
+				side = sides[j];
+				begin--;
+				wait1Msec(250);
+			}
+			break;
+	}
+}
+
+task chooser() {
+	initializeRobot();
+	while (true) {
+		startupCheck();
+		if (nNxtButtonPressed == 3) {
+			toggleMenu();
+			wait1Msec(250);
+		}
+		else if (nNxtButtonPressed == 2 || nNxtButtonPressed == 1) {
+			toggleSubMenu(nNxtButtonPressed);
+			wait1Msec(250);
+		}
+
+		if (toggle == 0) {
+			displayTextLine(0, "*Pgm: %s", file);
+			displayTextLine(1, " Delay: %2i", suspend);
+			displayTextLine(2, " Side: %s", side);
+
+			displayTextLine(4, " NXT Batt: %3f", (float)nAvgBatteryLevel / 1000);
+			displayTextLine(5, " EXT Batt: %3f", (float)externalBattery / 1000);
+		}
+		else if (toggle == 1) {
+			displayTextLine(0, " Pgm: %s", file);
+			displayTextLine(1, "*Delay: %2i", suspend);
+			displayTextLine(2, " Side: %s", side);
+
+			displayTextLine(4, " NXT Batt: %3f", (float)nAvgBatteryLevel / 1000);
+			displayTextLine(5, " EXT Batt: %3f", (float)externalBattery / 1000);
+		}
+
+		else if (toggle == 2) {
+			displayTextLine(0, " Pgm: %s", file);
+			displayTextLine(1, " Delay: %2i", suspend);
+			displayTextLine(2, "*Side: %s", side);
+
+			displayTextLine(4, " NXT Batt: %3f", (float)nAvgBatteryLevel / 1000);
+			displayTextLine(5, " EXT Batt: %3f", (float)externalBattery / 1000);
+		}
+		wait1Msec(5);
+		eraseDisplay();
+	}
+}
---
+++ autonomous_methods.h
@@ -0,0 +1,38 @@
+
+#include "libs.h";
+
+//autonomous_methods.h
+
+void initializeRobot() {
+	nVolume = 4;
+	latch(false);
+}
+
+// Finds the ir beacon, dumps the block, and parks on the ramp
+void bare(int suspend, int begin) {
+	delay(suspend * 100);
+	if (begin == 1){  //Parking Zone
+	}
+	if (begin == 0){  //Ramp
+		playSound(soundBeepBeep);
+		moveTo(-10, -3900, 2.1);
+		latch(true);
+		arcTurn(17, 30);
+		moveTo(15, 700, 0.8, 2000);
+		turn(30, 90);
+		turn(30, 90);
+		moveTo(-25, -3000, 2.0);
+		latch(false);
+	}
+
+}
+
+void autoFrontDump(int suspend) {
+	delay(suspend * 100);
+
+}
+
+void test(int suspend){
+	delay(suspend * 100);
+
+}
