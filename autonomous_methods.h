dc86b44241b0fb5da711166ff7b9f1950b06caa7
jspspike <jspspike@gmail.com>
2014-12-08 17:29:04 -0600

    Did some testing

--- autonomous_methods.h
+++ autonomous_methods.h
@@ -29,15 +29,15 @@ void bare(int s, int position) {
 	}
 	if (position == 0) {  //Ramp
 		playSound(soundBeepBeep);
-		moveTo(-10, -4895, 1.5, 4000);
+		moveTo(-20, -4895, 1.5, 4000);
 		turn(20, -45);
-		moveTo(-15, -699);
+		moveTo(-30, -4000);
 		turn(-20, -80);
-		moveTo(-15, -854, 2.0);
+		moveTo(-30, -6000, 2.0);
 		latch(true);
-		moveTo(15, 700);
+		moveTo(30, 700);
 		turn(20, 165);
-		moveTo(-40, -5516, 2.0);
+		moveTo(-80, -5516, 2.0);
 		lift(60, 2100);
 		basket('x');
 		latch(false);
