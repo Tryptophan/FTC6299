0e54fd39bb8170880065366e9a8810cadce81e4f
jspspike <jspspike@gmail.com>
2015-02-16 16:09:41 -0600

    "merged" autonomous

---
+++ autonomous_methods.h
@@ -0,0 +1,119 @@
+#include "libs.h";
+
+void initializeRobot() {
+	nVolume = 4;
+	latch(false);
+	basket('y');
+}
+
+// Finds the ir beacon, dumps the block, and parks on the ramp
+void bare(int s, int position, int tube) {
+	delay(s * 1000);
+	if (tube == 2) {
+		if (position == 1) {  //Parking Zone
+			playSound(soundBeepBeep);
+			moveTo(-25, -1243, 1.5);
+			turn(-25, -21);
+			moveTo(-25, -3885, 1.5);
+			turn(-20, -50);
+			moveTo(-25, -3900, 10.0, 2250);
+			moveTo(25, 1010);
+			turn(-25, 55);
+			moveTo(-20, -1700, 1.5);
+			latch(true);
+			moveTo(20, 1087, 1.5);
+			turn(25, 190);
+			moveTo(-50, -5439, 1.5);
+			lift(60, 2000);
+			basket('x');
+		}
+		if (position == 0) {  //Ramp
+			playSound(soundBeepBeep);
+			moveTo(-35, -4800, 1.5);
+			turn(50, -45);
+			moveTo(-30, -1200, 1.5);
+			turn(-50, -90);
+			grabMove(-25, -1000, 870, 1.5);
+			startTask(liftTaskB);
+			moveTo(50, 200, 1.5);
+			turn(-45, -75);
+			delay(2000);
+			basket('y');
+			moveTo(-40, -300, 1.5);
+			lift(-60, 2200);
+			latch(false);
+			moveTo(40, 400, 1.5);
+			turn(-60, 30);
+			moveTo(-50, -1100, 30.0);
+			moveTo(40, 100);
+			turn(-30, 85);
+			grabMove(-40, -750, 650, 1.5);
+			moveTo(50, 800);
+			turn(-42, 145);
+			startTask(liftTaskC);
+			moveTo(-100, -6800);
+			delay(500);
+			basket('x');
+		}
+	}
+	if (tube == 1) {
+		if (position == 0) {
+			playSound(soundBeepBeep);
+			moveTo(-15, -5050, 1.25, 4000);
+			turn(25, -45);
+			moveTo(-30, -1870, 1.0);
+			turn(25, 46);
+			moveTo(-25, -1600, 1.0);
+			latch(true);
+			moveTo(30, 4000, 1.0);
+			turn(25, 60);
+			moveTo(25, 375, 1.0);
+			turn(25, 145);
+			moveTo(-80, -3799, 1.5);
+			lift(60, 1270);
+			basket('x');
+			latch(false);
+		}
+	}
+	playSound(soundDownwardTones);
+}
+
+void center(int s, int position) {
+	if (position == 0) {
+		moveTo(15, 2000, 0.4);
+		displayTextLine(0, "%1i", getPos());
+		if (getPos() == 3) {
+			moveTo(-50, -300, 0.4);
+			turn(30, -90);
+			moveTo(50, 300, 0.4);
+			turn(30, -80);
+			moveTo(-50, -1200, 90.0, 1500);
+		}
+		else if (getPos() == 2) {
+			arcTurn(-50, -120);
+			moveTo(-40, -1540, 0.4);
+			turn(35, 80);
+			moveTo(-30, -1100);
+			moveTo(50, 125, 0.4);
+		}
+		else if (getPos() == 1) {
+			turn(-35, -110);
+			moveTo(-35, -3000, 0.4);
+			turn(-35, -50);
+			moveTo(-50, -600, 0.4);
+			turn(35, 80);
+			moveTo(-70, -600);
+			moveTo(50, 70, 0.4);
+		}
+		lift(60, 5000, 3500);
+		basket('x');
+		delay(2500);
+		basket('y');
+		lift(-60, 5000);
+	}
+}
+
+void kick(int suspend){
+	delay(suspend * 100);
+
+}
---
+++ libs.h
@@ -0,0 +1,403 @@
+#include "drivers\hitechnic-gyro.h";
+#include "drivers\hitechnic-irseeker-v2.h"
+
+/*
+Made by Team 6299 QuadX
+- Jacob Greenway
+- Joshua Johnson
+- Linnea May
+*/
+float heading = 0;
+
+float valInRange(float val, float threshold = 1.0) {
+return (abs(val) <= threshold) ? 0 : val;
+}
+
+bool isInRange(float heading, float targetHeading, float threshold = 1.0) {
+	return abs(heading - targetHeading) <= threshold;
+}
+
+int getLiftAverage() {
+	int divide = 2;
+	if (nMotorEncoder[liftL] == 0) {
+		divide--;
+	}
+	if (nMotorEncoder[liftR] == 0) {
+		divide--;
+	}
+	return /*((nMotorEncoder[liftL] + */nMotorEncoder[liftR]/*) / divide)*/;
+}
+
+int getEncoderAverage() {
+	int divide = 4;
+	if (nMotorEncoder[motorFL] == 0) {
+		divide--;
+	}
+	if (nMotorEncoder[motorBL] == 0) {
+		divide--;
+	}
+	if (nMotorEncoder[motorFR] == 0) {
+		divide--;
+	}
+	if (nMotorEncoder[motorBR] == 0) {
+		divide--;
+	}
+	return (nMotorEncoder[motorFL] + nMotorEncoder[motorBL] + nMotorEncoder[motorFR] + nMotorEncoder[motorBR]) / divide;
+}
+
+void setMotors(int left, int right) {
+	motor[motorFL] = left;
+	motor[motorBL] = left;
+	motor[motorFR] = right;
+	motor[motorBR] = right;
+}
+
+void stopMotors() {
+	motor[motorFL] = 0;
+	motor[motorBL] = 0;
+	motor[motorFR] = 0;
+	motor[motorBR] = 0;
+}
+
+void moveTo(int power, int deg, float threshold = 2.0, long time = 100000, float cor = 4.0) {
+	heading = 0;
+	nMotorEncoder[motorBL] = 0;
+	nMotorEncoder[motorFL] = 0;
+	nMotorEncoder[motorFR] = 0;
+	nMotorEncoder[motorBR] = 0;
+	wait1Msec(250);
+	clearTimer(T1);
+	if (power > 0) {
+		while (time1[T1] < time && getEncoderAverage() < deg) {
+			// Reads gyros rate of turn, mulitplies it by the time passed (20ms), and adds it to the current heading
+			heading += valInRange(HTGYROreadRot(SENSOR_GYRO), threshold) * (float)(20 / 1000.0);
+
+			// Checks if the gyro is outside of the specified threshold (1.0)
+			if (isInRange(heading, 0, threshold)) {
+				setMotors(power, power);
+			}
+
+			// If not, lower the speed of the required side of the robot to adjust back to 0
+			else {
+				if (heading > 0) {
+					setMotors((power / cor), power);
+				}
+				if (heading < 0) {
+					setMotors(power, (power / cor));
+				}
+			}
+			wait1Msec(20);
+		}
+	}
+
+	else {
+		while (time1[T1] < time && getEncoderAverage() > deg) {
+			// Reads gyros rate of turn, mulitplies it by the time passed (20ms), and adds it to the current heading
+			heading += valInRange(HTGYROreadRot(SENSOR_GYRO), threshold) * (float)(20 / 1000.0);
+
+			// Checks if the gyro is outside of the specified threshold (1.0)
+			if (isInRange(heading, 0, threshold)) {
+				setMotors(power, power);
+			}
+
+			// If not, lower the speed of the required side of the robot to adjust back to 0
+			else {
+				if (heading > 0) {
+					setMotors(power, (power / cor));
+				}
+				if (heading < 0) {
+					setMotors((power / cor), power);
+				}
+			}
+
+			wait1Msec(20);
+		}
+	}
+
+	stopMotors();
+}
+
+void turn(int power, int deg, int time = 6000) {
+
+	heading = 0;
+	wait1Msec(250);
+	clearTimer(T1);
+
+	if (deg > 0) {
+		while (time1[T1] < time && abs(heading) < abs(deg)) {
+			heading += HTGYROreadRot(SENSOR_GYRO) * (float)(20 / 1000.0);
+			setMotors(power, -power);
+			wait1Msec(20);
+		}
+	}
+
+	if (deg < 0) {
+		while (time1[T1] < time && abs(heading) < abs(deg)) {
+			heading += HTGYROreadRot(SENSOR_GYRO) * (float)(20 / 1000.0);
+			setMotors(-power, power);
+			wait1Msec(20);
+		}
+	}
+
+	stopMotors();
+}
+
+void arcTurn(int power, int deg, int time = 7000) {
+
+	// 90 Degree Modifier
+	if (abs(deg) == 90) {
+		int modifier = deg * 8/9;
+		deg = modifier;
+	}
+
+	// 45 Degree Modifier
+	else if (abs(deg) == 45) {
+		int modifier = deg * 7/9;
+		deg = modifier;
+	}
+
+	heading = 0;
+	wait1Msec(250);
+	clearTimer(T1);
+	// Forward arcTurn
+	if (power > 0) {
+		if (deg > 0) {
+			while (time1[T1] < time && abs(heading) < abs(deg)) {
+				heading += HTGYROreadRot(SENSOR_GYRO) * (float)(20 / 1000.0);
+				setMotors(power, 0);
+				wait1Msec(20);
+			}
+		}
+
+		else {
+			while (time1[T1] < time && abs(heading) < abs(deg)) {
+				heading += HTGYROreadRot(SENSOR_GYRO) * (float)(20 / 1000.0);
+				setMotors(0, power);
+				wait1Msec(20);
+			}
+		}
+	}
+
+	// Backward arcTurn (flips inequalities)
+	else {
+		if (deg > 0) {
+			while (time1[T1] < time && abs(heading) < abs(deg)) {
+				heading += HTGYROreadRot(SENSOR_GYRO) * (float)(20 / 1000.0);
+				setMotors(power, 0);
+				wait1Msec(20);
+			}
+		}
+
+		else {
+			while (time1[T1] < time && abs(heading) < abs(deg)) {
+				heading += HTGYROreadRot(SENSOR_GYRO) * (float)(20 / 1000.0);
+				setMotors(0, power);
+				wait1Msec(20);
+			}
+		}
+	}
+	stopMotors();
+}
+
+void drift(int power, int deg, int angle, int time = 8000) {
+	heading = 0;
+	nMotorEncoder[motorFL] = 0;
+	nMotorEncoder[motorFR] = 0;
+	wait1Msec(250);
+	clearTimer(T1);
+	if (power > 0) {
+		while (time1[T1] < time && getEncoderAverage() < deg) {
+			heading += HTGYROreadRot(SENSOR_GYRO) * (float)(20 / 1000.0);
+			if (angle > 0) {
+				while (heading < angle) {
+					setMotors(power, power - (power / 10));
+				}
+			}
+			else {
+				while (heading > angle) {
+					setMotors(power - (power / 10), power);
+				}
+			}
+		}
+	}
+	if (power < 0) {
+		while (time1[T1] < time && getEncoderAverage() < deg) {
+			heading += HTGYROreadRot(SENSOR_GYRO) * (float)(20 / 1000.0);
+			if (angle > 0) {
+				while (heading < angle) {
+					setMotors(power, power - 10);
+				}
+			}
+			else {
+				while (heading > angle) {
+					setMotors(power - 10, power);
+				}
+			}
+		}
+	}
+	stopMotors();
+}
+
+void latch(bool position) {
+	wait1Msec(250);
+	if (!position) {
+		servo[servoL] = 225;
+		servo[servoR] = 20;
+	}
+	if (position) {
+		servo[servoL] = 150;
+		servo[servoR] = 93;
+	}
+	wait1Msec(250);
+}
+
+int getPos() {
+	delay(450);
+	/*int count[5] = {0, 0, 0, 0, 0};
+	for (int j = 0; j < 20; j++) {
+	if (HTIRS2readACDir(SENSOR_IR) == 0) {
+	count[0] += 1;
+	}
+	if (HTIRS2readACDir(SENSOR_IR) == 1) {
+	count[1] += 1;
+	}
+	if (HTIRS2readACDir(SENSOR_IR) == 2) {
+	count[2] += 1;
+	}
+	if (HTIRS2readACDir(SENSOR_IR) == 3) {
+	count[3] += 1;
+	}
+	if (HTIRS2readACDir(SENSOR_IR) == 4) {
+	count[4] += 1;
+	}
+	delay(20);
+	}
+	int big = count[0];*/
+	int spot = HTIRS2readACDir(SENSOR_IR);
+	/*for (int j = 1; j < 5; j++) {
+	if (count[j] > big) {
+	spot = j;
+	big = count[j];
+	}
+	}*/
+	if (spot > 3) {
+		return 3;
+	}
+	else if (spot <= 3 && spot >= 1) {
+		return 2;
+	}
+	else {
+		return 1;
+	}
+}
+
+void lift(int power, int deg, int time = 6000) {
+	nMotorEncoder[liftL] = 0;
+	nMotorEncoder[liftR] = 0;
+	int t = 0;
+	wait1Msec(250);
+	while (t < time && abs(getLiftAverage()) < deg) {
+		motor[liftL] = power;
+		motor[liftR] = power;
+		t += 20;
+		wait1Msec(20);
+	}
+	motor[liftL] = 0;
+	motor[liftR] = 0;
+}
+
+void basket(char position) {
+
+	if (position == 'x') {
+		servo[liftServoL] = 100;
+		servo[liftServoR] = 155;
+	}
+	if (position == 'y') {
+		servo[liftServoL] = 220;
+		servo[liftServoR] = 25;
+	}
+}
+
+void manipulator(int time) {
+	motor[flip] = -100;
+	delay(time);
+	motor[flip] = 0;
+}
+
+void grabMove(int power, int deg, int lat, float threshold = 2.0, long time = 100000, float cor = 4.0) {
+	heading = 0;
+	nMotorEncoder[motorBL] = 0;
+	nMotorEncoder[motorFL] = 0;
+	nMotorEncoder[motorFR] = 0;
+	nMotorEncoder[motorBR] = 0;
+	wait1Msec(250);
+	clearTimer(T1);
+	if (power > 0) {
+		while (time1[T1] < time && getEncoderAverage() < deg) {
+			// Reads gyros rate of turn, mulitplies it by the time passed (20ms), and adds it to the current heading
+			heading += valInRange(HTGYROreadRot(SENSOR_GYRO), threshold) * (float)(20 / 1000.0);
+			if (lat < abs(getEncoderAverage())) {
+				latch(true);
+			}
+			// Checks if the gyro is outside of the specified threshold (1.0)
+			if (isInRange(heading, 0, threshold)) {
+				setMotors(power, power);
+			}
+
+			// If not, lower the speed of the required side of the robot to adjust back to 0
+			else {
+				if (heading > 0) {
+					setMotors((power / cor), power);
+				}
+				if (heading < 0) {
+					setMotors(power, (power / cor));
+				}
+			}
+			wait1Msec(20);
+		}
+	}
+
+	else {
+		while (time1[T1] < time && getEncoderAverage() > deg) {
+
+			// Reads gyros rate of turn, mulitplies it by the time passed (20ms), and adds it to the current heading
+			heading += valInRange(HTGYROreadRot(SENSOR_GYRO), threshold) * (float)(20 / 1000.0);
+
+			if (lat < abs(getEncoderAverage())) {
+				latch(true);
+			}
+			// Checks if the gyro is outside of the specified threshold (1.0)
+			if (isInRange(heading, 0, threshold)) {
+				setMotors(power, power);
+			}
+
+			// If not, lower the speed of the required side of the robot to adjust back to 0
+			else {
+				if (heading > 0) {
+					setMotors(power, (power / cor));
+				}
+				if (heading < 0) {
+					setMotors((power / cor), power);
+				}
+			}
+
+			wait1Msec(20);
+		}
+	}
+	stopMotors();
+	if(lat > getEncoderAverage()) {
+		latch(true);
+	}
+}
+
+task liftTaskC() {
+	manipulator(600);
+	lift(60, 3600);
+	stopTask(liftTaskC);
+}
+
+task liftTaskB() {
+	lift(60, 2200);
+	basket('x');
+	stopTask(liftTaskB);
+}
